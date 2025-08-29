a0f69fcbcd91bbd5a93427bd450cd7136b6754b6
redvampir <93953323+redvampir@users.noreply.github.com>
2025-08-29 07:32:28 +0300

    Bound diagnostics history with fixed window

--- backend/tests/diagnostics_anomaly_test.rs
+++ backend/tests/diagnostics_anomaly_test.rs
@@ -1,4 +1,4 @@
-use backend::action::diagnostics_node::DiagnosticsNode;
+use backend::action::diagnostics_node::{DiagnosticsNode, MAX_HISTORY};
 use backend::action::metrics_collector_node::MetricsRecord;
 use backend::analysis_node::QualityMetrics;
 use tokio::sync::mpsc::unbounded_channel;
@@ -9,16 +9,24 @@ async fn diagnostics_emits_alert_on_anomaly() {
     let (tx, rx) = unbounded_channel();
     let (_node, _dev_rx, mut alert_rx) = DiagnosticsNode::new(rx, 5);
 
-    for (i, val) in [1.0, 1.0, 1.0, 10.0].into_iter().enumerate() {
+    for i in 0..MAX_HISTORY {
         tx.send(MetricsRecord {
             id: format!("m{}", i),
             metrics: QualityMetrics {
-                credibility: Some(val),
+                credibility: Some(1.0),
                 ..Default::default()
             },
         })
         .unwrap();
     }
+    tx.send(MetricsRecord {
+        id: format!("m{}", MAX_HISTORY),
+        metrics: QualityMetrics {
+            credibility: Some(10.0),
+            ..Default::default()
+        },
+    })
+    .unwrap();
 
     let alert = timeout(Duration::from_millis(100), alert_rx.recv())
         .await
